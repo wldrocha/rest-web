e608626d82a0a79b8b26566fc8fb61228739d94e
Wladimir Rocha <wld.rocha@gmail.com>
2024-07-07 22:43:00 -0400

    feat: ✨ add basic HTTP server functionality

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('hi world');
+import http from 'http'
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write('<h1>Hello World</h1>')
+    res.end()
+ 
+})
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080')
+})
