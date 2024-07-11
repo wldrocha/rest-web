b43040ded9b52d8b05185e4e9a536d5d1b68bd59
Wladimir Rocha <wld.rocha@gmail.com>
2024-07-11 17:21:39 -0400

    chore: 🔄 Update .gitignore and package.json, add envs.ts and modify app.ts

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,20 +1,33 @@
 import express from 'express'
 import path from 'path'
 
+interface Options {
+  port: number
+  public_path?: string
+}
+
 export class Server {
   private app = express()
+
+  private readonly port: number
+  private readonly publicPath: string
+
+  constructor({ port, public_path = 'public' }: Options) {
+    this.port = port
+    this.publicPath = public_path
+  }
 
   async start() {
     // middleware or functions that execute when the server receives a request
 
     //  public folder
-    this.app.use(express.static('public'))
+    this.app.use(express.static(this.publicPath))
     this.app.get('*', (req, res) => {
       // show app when the server receives a request
-      const indexPath = path.join(`${__dirname}../../../public/index.html`)
+      const indexPath = path.join(`${__dirname}../../../${this.publicPath}/index.html`)
       res.sendFile(indexPath)
     })
 
-    this.app.listen(3000, () => console.log('Server running at http://localhost:3000'))
+    this.app.listen(this.port, () => console.log(`Server running at http://localhost:${this.port}`))
   }
 }
