cdc036b359943ed9014f78cb3a065f5b67d683ac
Wladimir Rocha <wld.rocha@gmail.com>
2024-07-12 23:05:33 -0400

    refactor: ♻️ Improve TodoEntity.fromObject method
    The TodoEntity.fromObject method has been updated to handle the case when the completedAt property is provided as a string. It now converts the string to a valid Date object before assigning it to the newCompletedAt variable. This ensures that the completedAt property is always a valid date.
    
    This commit refactors the TodoEntity.fromObject method to improve its functionality and reliability.

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -4,4 +4,19 @@ export class TodoEntity {
   get isCompleted() {
     return !!this.completedAt
   }
+
+  public static fromObject(obj: { [key: string]: any }): TodoEntity {
+    const { id, text, completedAt } = obj
+    if (!id) throw 'Id is required'
+    if (!text) throw 'Text is required'
+
+    let newCompletedAt
+    if (completedAt) {
+      newCompletedAt = new Date(completedAt)
+      if (isNaN(newCompletedAt.getTime())) {
+        throw 'COmpletedAt is not a valid date'
+      }
+    }
+    return new TodoEntity(id, text, newCompletedAt)
+  }
 }
