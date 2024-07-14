43d316ab09e37b3f38a9c9a7625e88caf3cfd1df
Wladimir Rocha <wld.rocha@gmail.com>
2024-07-13 22:12:45 -0400

    test: ✅ test on app

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,8 +1,18 @@
+import { envs } from '../src/config/envs'
+import { Server } from '../src/presentation/server'
 
+jest.mock('../src/presentation/server')
 
-describe('Testing App.ts', ()=>{
-
-    test('should word', ()=>{
-        expect(true).toBeTruthy()
+describe('Testing App.ts', () => {
+  test('should word', async () => {
+    await import('../src/app')
+    expect(Server).toHaveBeenCalledTimes(1)
+    expect(Server).toHaveBeenCalledWith({
+      port: envs.PORT,
+      public_path: envs.PUBLIC_PATH,
+      routes: expect.any(Function)
     })
+
+    expect(Server.prototype.start).toHaveBeenCalledWith()
+  })
 })
