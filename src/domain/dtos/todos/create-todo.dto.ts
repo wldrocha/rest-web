c7b7283a42e2631aba5a793433f0a357cecb5d9b
Wladimir Rocha <wld.rocha@gmail.com>
2024-08-08 12:01:16 -0400

    refactor: ♻️ revalidate after create test

--- src/domain/dtos/todos/create-todo.dto.ts
+++ src/domain/dtos/todos/create-todo.dto.ts
@@ -1,8 +1,8 @@
 export class CreateTodoDto {
   private constructor(public readonly text: string) {}
   static create(props: { [key: string]: any }): [string?, CreateTodoDto?] {
-    const {text} = props
-    if(!text) return ['Text property is required', undefined]
+    const { text } = props
+    if (!text || text.length < 1) return ['Text property is required', undefined]
     return [undefined, new CreateTodoDto(text)]
   }
 }
