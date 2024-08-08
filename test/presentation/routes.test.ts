8438e803474393d4b2de32572569fba960bb2784
Wladimir Rocha <wld.rocha@gmail.com>
2024-08-08 11:05:46 -0400

    chore: 🧪 get all todos

--- test/presentation/routes.test.ts
+++ test/presentation/routes.test.ts
@@ -1,17 +1,32 @@
 import request from 'supertest'
 import { testServer } from '../test-server'
-import test from 'node:test'
+import { prisma } from '../../src/data'
 
 describe('Todo route testing', () => {
   beforeAll(async () => {
     await testServer.start()
   })
 
-  afterAll(async () => {
+  afterAll(() => {
     testServer.close()
   })
-  test('should return TODOS', async () => {
-    const response = await request(testServer.app).get('/api/todos').expect(200)
-    console.log('🚀 ~ test ~ response.body:', response.body)
+
+  const todo1 = { text: 'todo 1' }
+  const todo2 = { text: 'todo 2' }
+  test('should return TODOS api/todos', async () => {
+    await prisma.todo.deleteMany()
+    await prisma.todo.createMany({
+      data: [todo1, todo2]
+    })
+    const { body } = await request(testServer.app).get('/api/todos').expect(200)
+
+    expect(body).toBeInstanceOf(Array)
+    expect(body.length).toBe(2)
+
+    expect(body[0].text).toBe(todo1.text)
+    expect(body[1].text).toBe(todo2.text)
+    expect(body[0].completedAt).toBeUndefined()
   })
+
+
 })
