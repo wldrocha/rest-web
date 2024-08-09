83e8e7441fb0a7b10c7d4e29454bd42daf216c43
Wladimir Rocha <wld.rocha@gmail.com>
2024-08-08 20:51:36 -0400

    test: ✅ config test when delete todo

--- test/presentation/routes.test.ts
+++ test/presentation/routes.test.ts
@@ -110,4 +110,22 @@ describe('Todo route testing', () => {
       completedAt: undefined
     })
   })
+
+  test('should to delete a TODO api/todos/:id', async () => {
+    const todo = await prisma.todo.create({ data: todo1 })
+
+    const { body } = await request(testServer.app).delete(`/api/todos/${todo.id}`).expect(200)
+
+    expect(body).toEqual({
+      id: todo.id,
+      text: todo.text,
+      completedAt: undefined
+    })
+  })
+
+  test('should return 404 if todo do not exist api/todos/:id', async () => {
+    const { body } = await request(testServer.app).delete(`/api/todos/${todoIdNotExist}`).expect(400)
+
+    expect(body).toEqual({ error: `Todo with id ${todoIdNotExist} not found` })
+  })
 })
